de948097421e15d45072a455afedc0c1efc6c269
Verdick <verdick@gmail.com>
2026-03-20 08:53:21 +0200

    fix mongoose options

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
   try {
     console.log("Connexion MongoDB...");
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connecté");
 
